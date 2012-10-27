f3ed5dff2d2f302cf4d364a4ec26f6ecfd95c049
Rafael I <rafael.ibanez@auone.jp>
2012-10-26 20:31:35 -0400

    initial commit

---
+++ RedirectBuilder/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RedirectBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnOpenFileDialog = new System.Windows.Forms.Button();
+            this.btnCreateRedirect = new System.Windows.Forms.Button();
+            this.lblFileLocation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(79, 29);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(302, 20);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // btnOpenFileDialog
+            // 
+            this.btnOpenFileDialog.Location = new System.Drawing.Point(396, 29);
+            this.btnOpenFileDialog.Name = "btnOpenFileDialog";
+            this.btnOpenFileDialog.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFileDialog.TabIndex = 1;
+            this.btnOpenFileDialog.Text = "&Browse...";
+            this.btnOpenFileDialog.UseVisualStyleBackColor = true;
+            this.btnOpenFileDialog.Click += new System.EventHandler(this.btnOpenFileDialog_Click);
+            // 
+            // btnCreateRedirect
+            // 
+            this.btnCreateRedirect.Location = new System.Drawing.Point(491, 29);
+            this.btnCreateRedirect.Name = "btnCreateRedirect";
+            this.btnCreateRedirect.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateRedirect.TabIndex = 2;
+            this.btnCreateRedirect.Text = "&Create Redirect";
+            this.btnCreateRedirect.UseVisualStyleBackColor = true;
+            this.btnCreateRedirect.Click += new System.EventHandler(this.btnCreateRedirect_Click);
+            // 
+            // lblFileLocation
+            // 
+            this.lblFileLocation.AutoSize = true;
+            this.lblFileLocation.Location = new System.Drawing.Point(16, 32);
+            this.lblFileLocation.Name = "lblFileLocation";
+            this.lblFileLocation.Size = new System.Drawing.Size(57, 13);
+            this.lblFileLocation.TabIndex = 3;
+            this.lblFileLocation.Text = "File Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 81);
+            this.Controls.Add(this.lblFileLocation);
+            this.Controls.Add(this.btnCreateRedirect);
+            this.Controls.Add(this.btnOpenFileDialog);
+            this.Controls.Add(this.txtFileName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Redirect Builder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnOpenFileDialog;
+        private System.Windows.Forms.Button btnCreateRedirect;
+        private System.Windows.Forms.Label lblFileLocation;
+    }
+}
+
---
+++ RedirectBuilder/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RedirectBuilder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.Filter = "MS Project Files (*.mpp)|*.mpp|" + "All files (*.*)|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+                txtFileName.Text = dlg.FileName;
+        }
+
+        private void btnCreateRedirect_Click(object sender, EventArgs e)
+        {
+            String strFileNameAll = System.IO.Path.GetFileName(txtFileName.Text);
+            String strFileNameOnly = System.IO.Path.GetFileNameWithoutExtension(txtFileName.Text);
+            String strNewFile = strFileNameAll + ".html";
+
+            // File fNewHtml = new File(strFileNameAll + ".html");
+
+            FileStream outputfile = null;
+
+            try
+            {
+                outputfile = new FileStream(strNewFile, FileMode.OpenOrCreate, FileAccess.Write);
+
+
+                StreamWriter writer = new StreamWriter(outputfile);
+
+                writer.BaseStream.Seek(0, SeekOrigin.End);
+
+                writer.WriteLine("<Script type=\"text/javascript\">");
+                writer.Flush();
+                writer.WriteLine("var projectFile = \"V:\\\\FmsCore\\\\ProjMgmt\\\\01_ProgMgmt\\\\01_ScheduleMgmt\\\\SubProjects\\\\" + strFileNameAll + "\";");
+                writer.Flush();
+                writer.WriteLine("window.location = \"file://\" + projectFile;");
+                writer.Flush();
+                writer.WriteLine("</Script><html xmlns:mso=\"urn:schemas-microsoft-com:office:office\" xmlns:msdt=\"uuid:C2F41010-65B3-11d1-A29F-00AA00C14882\"><head>");
+                writer.Flush();
+                writer.WriteLine("<!--[if gte mso 9]><xml>");
+                writer.Flush();
+                writer.WriteLine("<mso:CustomDocumentProperties>");
+                writer.Flush();
+                writer.WriteLine("<mso:Doc_x0020_Cat msdt:dt=\"string\">Project Management</mso:Doc_x0020_Cat>");
+                writer.Flush();
+                writer.WriteLine("<mso:Deliverable msdt:dt=\"string\">0</mso:Deliverable>");
+                writer.Flush();
+                writer.WriteLine("<mso:Tags msdt:dt=\"string\"></mso:Tags>");
+                writer.Flush();
+                writer.WriteLine("<mso:ContentType msdt:dt=\"string\">Document</mso:ContentType>");
+                writer.Flush();
+                writer.WriteLine("<mso:Doc_x0020_Type msdt:dt=\"string\">Project Schedule</mso:Doc_x0020_Type>");
+                writer.Flush();
+                writer.WriteLine("<mso:Document_x0020_Type msdt:dt=\"string\">;#In Progress;#</mso:Document_x0020_Type>");
+                writer.Flush();
+                writer.WriteLine("<mso:_Comments msdt:dt=\"string\"></mso:_Comments>");
+                writer.Flush();
+                writer.WriteLine("</mso:CustomDocumentProperties>");
+                writer.Flush();
+                writer.WriteLine("</xml><![endif]-->");
+                writer.Flush();
+                writer.WriteLine("<title>" + strFileNameAll + "</title></head>");
+                writer.Flush();
+
+                writer.Close();
+
+                MessageBox.Show("File Created Successfully!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Generate Code = " + ex);
+                outputfile = null;
+                // writer = null;
+
+                MessageBox.Show("Error!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
